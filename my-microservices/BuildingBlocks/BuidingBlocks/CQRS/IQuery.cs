80146a2321b738fdf875840a22a3eef78d00267f
nxson2509HD <nxson2509.dev@gmail.com>
2024-12-17 00:18:43 +0700

    init CQRS

---
+++ my-microservices/BuildingBlocks/BuidingBlocks/CQRS/IQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuidingBlocks.CQRS
+{
+    public interface IQuery<out TRespose> : IRequest<TRespose> where TRespose : notnull
+    {
+    }
+}
---
+++ my-microservices/BuildingBlocks/BuidingBlocks/CQRS/IQueryHandler.cs
@@ -0,0 +1,10 @@
+﻿using BuidingBlocks.CQRS;
+using MediatR;
+
+namespace BuildingBlocks.CQRS;
+public interface IQueryHandler<in TQuery, TResponse>
+    : IRequestHandler<TQuery, TResponse>
+    where TQuery : IQuery<TResponse>
+    where TResponse : notnull
+{
+}
